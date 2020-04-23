62c085672b78b066abe13c5bc35471ba9372a938
Zachary Luther <Zluther89@gmail.com>
2020-04-23 15:18:35 -0400

    prod mode

--- my-app/src/socket.js
+++ my-app/src/socket.js
@@ -1,4 +1,6 @@
 import io from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:1337";
 
-export const socket = io(ENDPOINT);
+// export const socket = io(ENDPOINT); //dev only
+
+export const socket = io();
